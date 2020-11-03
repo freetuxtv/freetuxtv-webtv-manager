dcd49f55d74baa736cfb5af7f55cf9199117a136
Eric Beuque <eric.beuque@gmail.com>
2020-11-03 23:26:52 +0100

    Add more spam clean rules

--- database-clean.sql
+++ database-clean.sql
@@ -12,3 +12,50 @@ DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHE
 
 DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%pharmacy%' ORDER BY `Id` DESC);
 DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%pharmacy%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%.ru %' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%.ru %' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%article%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%article%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%website%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%website%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%web site%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%web site%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%blog%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%blog%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%<%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%<%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%information%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%information%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%rss%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%rss%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%casino%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%casino%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%advice%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%advice%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%XRumer%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%XRumer%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%a lot %' AND HistoryId>40000 ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%a lot %' AND HistoryId>40000 ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%you%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%you%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%web page%' ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%web page%' ORDER BY `Id` DESC);
+
+DELETE FROM wtvmT_EditRequest WHERE HistoryId IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%.be%' AND HistoryId>40000 ORDER BY `Id` DESC);
+DELETE FROM wtvmT_History WHERE Id IN (SELECT HistoryId FROM `wtvmT_Comment` WHERE `Comment` LIKE '%.be%' AND HistoryId>40000 ORDER BY `Id` DESC);
+
+-- 2020/11/03 - HistoryId = 122816
